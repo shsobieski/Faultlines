14d2d4703d4c61025c4272c3425e48bf08e24b94
Shawn Sobieski <shsobieski@gmail.com>
2020-07-07 11:48:35 -0700

    downloaded weights

--- functions/unzip.py
+++ functions/unzip.py
@@ -2,5 +2,8 @@ def unzip(file):
     my_tar = tarfile.open('data/zipped_data/'+ file +'.tar.gz')
     my_tar.extractall('./data/unzipped_data/' + file)
     my_tar.close()
-    return pd.read_csv('data/unzipped_data/' 
+    df = pd.read_csv('data/unzipped_data/' 
                        + file + '/' + file.lower() + '.csv')
+    df.rename(columns = {'Gss year for this respondent                       ' : 'year', 'Respondent id number': 'id'}, inplace = True)
+    df['id'] = range(0, len(df['id']))
+    return df
