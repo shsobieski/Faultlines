2a96a868bc4e7d40a2ebf82c28d40be3d811d3ea
Shawn Sobieski <shsobieski@gmail.com>
2020-07-20 22:35:56 -0700

    wrote storage function for exporting predictions to tableau

--- functions/make_preds.py
+++ functions/make_preds.py
@@ -1,13 +1,17 @@
-def make_preds(ts):
+def make_preds(ts, name):
     ts.index = pd.to_datetime(overall['year'], format='%Y')
     ts.index = ts.index.to_period('Y')
-    best_aic = 10000000000000
-    best_combo = 0
+    ts.dropna(inplace = True)
+    try:
+        model = ARIMA(ts, (1,1,1))
+        arma = model.fit(disp=0)
+        best_aic = arma.aic
+        best_combo = (1,1,1)
+    except:
+        best_aic = 1000000000
+        best_combo = 0
     for combo in [(1,1,1),(1,1,2),(2,1,1),(2,1,2),(3,1,1),
-                  (3,1,2),(1,1,3),(2,1,3),(3,1,3),(4,1,1),
-                  (4,1,2),(4,1,1),(1,1,4),(2,1,4),(3,1,4),
-                  (4,1,4),(5,1,1),(5,1,2),(5,1,3),(5,1,4),
-                  (1,1,5),(2,1,5),(3,1,5),(4,1,5),(5,1,5)]:
+                  (3,1,2),(1,1,3),(2,1,3),(3,1,3)]:
         try:
             model = ARIMA(ts, order=combo)
             arma = model.fit(disp=0)
@@ -25,4 +29,5 @@ def make_preds(ts):
     preds = pd.Series(arma.forecast(6)[0])
     years = np.array(range(2019, 2025))
     preds.index = years
+    preds = pd.DataFrame(preds, columns = [name])
     return preds
---
+++ functions/store_predictions.py
@@ -0,0 +1,14 @@
+def store_predictions(df):
+    ts = df[df.columns[1]]
+    base = pd.DataFrame(ts)
+    preds = make_preds(ts, 'Predicted '+ df.columns[1])
+    base.index = df['year']
+    base = base.append(pd.DataFrame(preds), sort = True)
+    for col in df.columns[2:]:
+        ts = df[col]
+        temp = pd.DataFrame(ts)
+        preds = make_preds(ts, 'Predicted ' + col)
+        temp.index = df['year']
+        temp = temp.append(pd.DataFrame(preds), sort = True)
+        base = base.join(temp)
+    return base
